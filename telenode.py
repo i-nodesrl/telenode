5ffb9248cee990336d40dc2acc560cf179eec601
dpucci <d.pucci@i-node.it>
2018-11-16 12:25:53 +0100

    telenode: add broadcast message, increase logging and add users caching

--- telenode.py
+++ telenode.py
@@ -4,16 +4,37 @@ import inotify.adapters
 import inotify.constants
 import json
 import os
+import pickle
 import requests
 import sys
 import time
 import telepot
 from telepot.loop import MessageLoop
-from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+from telepot.namedtuple import ForceReply, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def on_chat_message(msg):
 	msg_content_type, msg_chat_type, msg_chat_id = telepot.glance(msg)
+
+	global bot_users
+	global bot_users_dump
+
+	if msg_chat_id not in bot_users:
+		print 'Registering {} ID to users library'.format(msg_chat_id)
+		bot_users.add(msg_chat_id)
+		print 'Writing {} users library to disk at {}'.format(list(bot_users), bot_users_dump)
+		json.dump(list(bot_users), open(bot_users_dump, 'wb'))
+
+	if 'reply_to_message' in msg:
+		print '[{}] Command callback for handwritted message'.format(msg_chat_id)
+		command_callback = msg['reply_to_message']['text'].split(':')[0].lower()
+		print '[{}] Command detected: {}'.format(msg_chat_id, command_callback)
+		try:
+			eval("callback_" + command_callback)(msg, msg_chat_id, None)
+		except NameError as error:
+			print '[{}] Unable to fire function {}'.format(msg_chat_id, "callback_" + command_callback)
+		return
+
 	msg_command = msg['text'].split()[0].strip().lower()
 	if len(msg_command) < 1 or msg_command[0] != '/' or msg_content_type != 'text':
 		return
@@ -23,13 +44,28 @@ def on_chat_message(msg):
 	if msg_command == 'start':
 		return
 	try:
+		print '[{}] Command recognized {}'.format(msg_chat_id, msg_command)
 		eval("command_" + msg_command)(msg, msg_chat_id)
 	except NameError as error:
+		print '[{}] Unable to fire function {}'.format(msg_chat_id, "command_" + msg_command)
 		bot.sendMessage(msg_chat_id, "Comando `{}` non eseguito: ```{}```".format(msg['text'], str(error)), parse_mode='Markdown')
 
 
+def on_callback_query(msg):
+	msg_query_id, msg_chat_id, msg_data = telepot.glance(msg, flavor='callback_query')
+	try:
+		eval("callback_" + msg_data.split(':')[0])(msg_data, msg_chat_id, msg_query_id)
+	except NameError as error:
+		bot.sendMessage(msg_chat_id, "Impossibile eseguire hook su `{}`".format(msg_data), parse_mode='Markdown')
+
+
 def command_ping(msg, msg_chat_id):
-	bot.sendMessage(msg_chat_id, "In esecuzione da {}".format('?'), parse_mode='Markdown')
+	global bot_start_time
+	delta = time.time() - bot_start_time
+	delta_h = delta // 3600
+	delta_m = delta // 60
+	delta_s = delta - (delta_m * 60)
+	bot.sendMessage(msg_chat_id, "Attivo. Tempo di esecuzione: {:02}:{:02}:{:02}.".format(int(delta_h), int(delta_m), int(delta_s)), parse_mode='Markdown')
 
 
 def command_ack(msg, msg_chat_id):
@@ -41,23 +77,9 @@ def command_ack(msg, msg_chat_id):
 		bot.sendMessage(msg_chat_id, "Nessun problema rilevato.", parse_mode='Markdown')
 
 
-def command_get(msg, msg_chat_id):
-	pass
-
-
 def command_broadcast(msg, msg_chat_id):
-	pass
-
-
-def on_callback_query(msg):
-	msg_query_id, msg_chat_id, msg_data = telepot.glance(msg, flavor='callback_query')
-	try:
-		eval("callback_" + msg_data.split(':')[0])(msg_data, msg_chat_id, msg_query_id)
-	except NameError as error:
-		bot.sendMessage(msg_chat_id, "Impossibile eseguire hook su `{}`".format(msg_data), parse_mode='Markdown')
-
-	# bot.sendMessage(msg_chat_id, msg_data, parse_mode='Markdown')
-	# bot.answerCallbackQuery(msg_query_id, text="YEAH")
+	markup = ForceReply()
+	bot.sendMessage(msg_chat_id, 'Broadcast: inserire un messaggio da inviare.', reply_markup=markup)
 
 
 def callback_ack(msg_data, msg_chat_id, msg_query_id):
@@ -66,12 +88,17 @@ def callback_ack(msg_data, msg_chat_id, msg_query_id):
 	bot.answerCallbackQuery(msg_query_id, text="Inviato acknowledgment di {} su {}".format(msg_data.split("!")[1], msg_data.split("!")[0]))
 
 
-def callback_get(msg, msg_chat_id, msg_query_id):
-	pass
+def callback_broadcast(msg_data, msg_chat_id, msg_query_id):
+	broadcast_sender = '{} {}'.format(msg_data['chat']['first_name'], msg_data['chat']['last_name'])
+	broadcast_sender_username = msg_data['chat']['username']
+	broadcast_content = msg_data['text']
 
-
-def callback_broadcast(msg, msg_chat_id, msg_query_id):
-	pass
+	global bot_users
+	for mid in bot_users:
+		if mid != msg_chat_id:
+			print '[{}] Sending broadcast message to {}'.format(msg_chat_id, mid)
+			bot.sendMessage(mid, "Messaggio da {} ({}):\n```{}```".format(broadcast_sender, broadcast_sender_username, broadcast_content), parse_mode='Markdown')
+	bot.sendMessage(msg_chat_id, "Messaggio broadcast inviato a {} contatto/i.".format(len(bot_users) - 1), parse_mode='Markdown')
 
 
 def icinga_do_ack(problem):
@@ -96,7 +123,6 @@ def icinga_get_hosts():
 
 
 def icinga_get_problems():
-	# TODO: remove already acknowledged
 	request_data = {
 		'filter': 'service.state != service_state && service.acknowledgement == service_ack && host.acknowledgement != host_ack',
 		'filter_vars': {
@@ -135,6 +161,8 @@ for env_key in ['TELEGRAM_BOT_TOKEN', 'ICINGA2_API_USER', 'ICINGA2_API_PWD']:
 		print "Mandatory environment key \"{}\" is empty.".format(env_key)
 		sys.exit(1)
 
+proc_path = os.path.abspath(os.path.dirname(sys.argv[0]))
+
 icinga2_host = os.environ['ICINGA2_HOST'] if 'ICINGA2_HOST' in os.environ else '127.0.0.1'
 icinga2_api_port = os.environ['ICINGA2_API_PORT'] if 'ICINGA2_API_PORT' in os.environ else '5665'
 
@@ -142,11 +170,21 @@ session = requests.Session()
 session.auth = (os.environ['ICINGA2_API_USER'], os.environ['ICINGA2_API_PWD'])
 
 bot = telepot.Bot(os.environ['TELEGRAM_BOT_TOKEN'])
+bot_users = set()
+bot_users_dump = "{}/{}".format(proc_path, '.users.json')
+
+print "Script is in {}".format(proc_path)
+if os.path.isfile(bot_users_dump):
+	print 'Reloading users library at {}'.format(bot_users_dump)
+	bot_users = set(json.load(open(bot_users_dump, 'rb')))
+	print 'Users library members: {}'.format(len(bot_users))
+
+bot_start_time = time.time()
 bot.message_loop({'chat': on_chat_message, 'callback_query': on_callback_query})
-# MessageLoop(bot, sort_message).run_as_thread()
 
 i = inotify.adapters.Inotify()
 i.add_watch(sys.argv[0], mask=inotify.constants.IN_MODIFY)
+i.add_watch(bot_users_dump, mask=inotify.constants.IN_MODIFY)
 for event in i.event_gen(yield_nones=False):
 	print "Reloading updated bot version"
 	os.execve(sys.argv[0], sys.argv, os.environ)
